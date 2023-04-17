355b5cad266f9ba040fea6f86a8f0b3cd6f4a47d
lina1197 <laa.nov97@outlook.com>
2023-04-17 13:10:06 +0200

    social media icons

--- src/components/Accueil.jsx
+++ src/components/Accueil.jsx
@@ -6,7 +6,17 @@ function Accueil() {
 <div><Navigation 
         />
         <ModeSombre/>
+        <div className="Intro"><h1>Hi,I'm Lina <br />A Full-Stack Web Developer</h1></div>
+      <div className="HomeIMG"><img  src="/images/home.png" alt="" /></div> 
+      <div className="SocialMedia" >
+<i class="fab fa-facebook-f fa-lg white-icon"></i>
+            <i class="fab fa-twitter fa-lg white-icon"></i>
+<i class="fab fa-instagram fa-lg white-icon"></i>
+<i class="fab fa-youtube fa-lg white-icon"></i>
+
+          </div> 
         </div>
+
 
   )}
 
